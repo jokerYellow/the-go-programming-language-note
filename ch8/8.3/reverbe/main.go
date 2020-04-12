739e5fd34ce193b63ce90d6a3540c1dab1dfe228
wandererswordsman <wandererswordsman@gmail.com>
2020-04-12 21:08:32 +0800

    [update]: close tcp write connect .

--- ch8/8.3/reverbe/main.go
+++ ch8/8.3/reverbe/main.go
@@ -10,17 +10,56 @@ import (
 
 func handleConn(conn net.Conn) {
 	input := bufio.NewScanner(conn)
+	requestTodo := make(chan int)
+	closedWrite := make(chan struct{})
+	requestDone := make(chan struct{})
+	log.Printf("%s:connected", conn.RemoteAddr().String())
+	go func() {
+		sum := 0
+		for c := range requestTodo {
+			sum += c
+			if sum == 0 && (c == 0 || c == -1) {
+				go func() {
+					requestDone <- struct{}{}
+				}()
+			}
+		}
+	}()
+	go func() {
+		<-closedWrite
+		<-requestDone
+		conn.(*net.TCPConn).CloseWrite()
+		log.Println("close write")
+	}()
 	for input.Scan() {
+		log.Println("new scan")
 		content := input.Text()
+		requestTodo <- 1
+		log.Println("new scan:", content)
 		go func() {
-			fmt.Fprintln(conn, content+"!")
-			time.Sleep(1 * time.Second)
-			fmt.Fprintln(conn, content+"!!")
-			time.Sleep(1 * time.Second)
-			fmt.Fprintln(conn, content+"!!!")
+			_, err := fmt.Fprintln(conn, content+"!")
+			if err != nil {
+				log.Println(err)
+			}
+			time.Sleep(5 * time.Second)
+			_, err = fmt.Fprintln(conn, content+"!!")
+			if err != nil {
+				log.Println(err)
+			}
+			time.Sleep(5 * time.Second)
+			_, err = fmt.Fprintln(conn, content+"!!!")
+
+			if err != nil {
+				log.Println(err)
+			}
+			requestTodo <- -1
 		}()
 	}
-	defer conn.Close()
+	log.Println("goto close read")
+	closedWrite <- struct{}{}
+	log.Println("close read")
+	requestTodo <- 0
+	conn.(*net.TCPConn).CloseRead()
 }
 
 func main() {
@@ -33,6 +72,6 @@ func main() {
 		if err != nil {
 			log.Fatal(err.Error())
 		}
-		handleConn(conn)
+		go handleConn(conn)
 	}
 }
